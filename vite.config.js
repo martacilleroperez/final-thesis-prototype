f72e2aa94af67f6895f2484cd9239bb8b23ae033
mc <m.cilleroperez@student.utwente.nl>
2026-06-12 12:13:52 +0200

    Add control and freedom pages as build entry points

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/final-thesis-prototype/'
+  base: '/final-thesis-prototype/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        control: resolve(__dirname, 'control.html'),
+        freedom: resolve(__dirname, 'freedom.html'),
+      },
+    },
+  },
 })
